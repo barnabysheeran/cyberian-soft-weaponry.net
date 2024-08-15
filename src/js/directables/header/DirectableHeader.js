6bd4c882b678c507f028047f47c00a175460ac37
Barnaby Sheeran <barnabysheeran@gmail.com>
2024-08-15 22:00:27 +0100

    chore: Refactor DirectableHeader class and add setHeaderText method

--- src/js/directables/header/DirectableHeader.js
+++ src/js/directables/header/DirectableHeader.js
@@ -1,9 +1,16 @@
 import Directable from '../Directable';
 
 export default class DirectableHeader extends Directable {
-	// __________________________________________________________________________
+	// _________________________________________________________________________
 
 	constructor() {
 		super();
+	}
+
+	// _________________________________________________________ Set Header Text
+
+	/* eslint-disable class-methods-use-this */
+	setHeaderText(text) {
+		document.title = text;
 	}
 }
